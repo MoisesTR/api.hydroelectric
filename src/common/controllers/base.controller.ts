10b8eb96f49d26de9d2bd5bdf14b85a1bb3279d8
Moises Trigueros <moisestrigueros@hotmail.com>
2020-08-15 16:26:42 -0600

    Add new custom sql exception and add logic for catching sql errors

--- src/common/controllers/base.controller.ts
+++ src/common/controllers/base.controller.ts
@@ -1,11 +1,19 @@
 import { Response } from 'express';
 import ApplicationException from '../exceptions/application.exception';
+import { SqlException, SqlError } from '../exceptions/sql.exception';
 
 export default abstract class BaseController {
     handleException(err: any, res: Response) {
         if (err instanceof ApplicationException) {
             res.status(400);
             res.send(err.message);
+        } else if (err instanceof SqlException) {
+            if (err.cause.number === 50000) {
+                res.status(400);
+                res.send(err.message);
+                return;
+            }
+            throw new Error(err.message);
         } else {
             throw new Error(err);
         }
